4091c63915f94260396adc1747ddd49b6387f2cf
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-03-26 22:27:26 +0530

    Setting Up DB and Styling Meals Page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,27 @@
-export default function MealsPage() {
-  return <div>Meals</div>;
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious Meal, created
+          <span className={styles.highlight}> by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipes</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
