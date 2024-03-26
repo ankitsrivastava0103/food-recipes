7ad6611bacca92fbf74346769995676f35687616
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-03-26 22:40:20 +0530

    Adding Loading Uisng Suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,15 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -20,7 +26,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
