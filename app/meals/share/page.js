ff5d6691c837fc230eecfb520a43344b485e695a
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-03-28 20:19:40 +0530

    Saving From Data On Server Side

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker";
 import styles from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meal-form-submit";
 
 export default function ShareMealPage() {
   return (
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +41,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your Iamge" name="image" />
           <p className={styles.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
